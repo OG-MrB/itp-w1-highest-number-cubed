4a703368b979cdca49e7c8ec4f6b8a2418a4aba8
Eric <efletcher001@gmail.com>
2017-03-24 01:28:25 +0000

    Initial Commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,22 @@
 """This is the entry point of the program."""
-
+# def highest_number_cubed(limit):
+#     num = 0
+#     cubed = 0
+#     while cubed < limit:
+#         cubed ** 3
+#         num +=1
+        
+#     return(limit) 
+       
+       
+'''       
+1 ** 3 = < limit? yes
+'''
 
 def highest_number_cubed(limit):
-    pass
+   Num = 0
+   CubeNum = 0
+   while CubeNum < limit:
+       Num += 1
+       CubeNum = Num**3
+   return Num-1
